c032f5144c9bf14b1fa834da648d7ab62765d837
Kiattipong Kamonrat <marsimadz@hotmail.com>
2023-12-06 23:10:49 +0700

    ✨ feat: add advent of typescript 2023 day 1

---
+++ 2023/01_Christmas_Cookies/app.ts
@@ -0,0 +1,12 @@
+// https://typehero.dev/challenge/day-1
+
+import { Expect, Equal } from 'type-testing';
+
+type SantasFavoriteCookies = 'ginger-bread' | 'chocolate-chip';
+
+type test_0_actual = SantasFavoriteCookies;
+type test_0_expected = 'ginger-bread' | 'chocolate-chip';
+type test_0 = Expect<Equal<test_0_actual, test_0_expected>>;
+
+var result: test_0 = true;
+console.log(result);
